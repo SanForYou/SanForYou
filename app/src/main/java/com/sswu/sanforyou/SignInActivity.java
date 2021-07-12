03fa8fe73d43d4b1bcb59d046876b8cc5aa039f7
Your Name <78153440+thisisyum@users.noreply.github.com>
2021-07-12 20:41:27 +0900

    feat: 로그인, 회원가입 액태비티 코드 추가 colors.xml 배경색 추가 첫화면 로그인화면으로 설정

---
+++ app/src/main/java/com/sswu/sanforyou/SignInActivity.java
@@ -0,0 +1,29 @@
+package com.sswu.sanforyou;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SignInActivity extends AppCompatActivity {
+
+    //로그인 버튼 : 회원가입 액티비티로 이동
+    private Button btn_sign_in;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        btn_sign_in = findViewById(R.id.btn_sign_in);
+        btn_sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignInActivity.this, SignUpActivity.class);
+                startActivity(intent); //액티비티 이동
+            }
+        });
+    }
+}
