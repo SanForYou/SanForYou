05e164bb80c0eb3ffc702898082836c0c8b679ed
안예원 <yewonahn@naver.com>
2021-07-12 20:31:40 +0900

    feat: info UI 추가

--- app/src/main/java/com/sswu/sanforyou/InfoFragment.java
+++ app/src/main/java/com/sswu/sanforyou/InfoFragment.java
@@ -1,12 +1,12 @@
 package com.sswu.sanforyou;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+
+import androidx.fragment.app.Fragment;
 
 public class InfoFragment extends Fragment {
 
@@ -14,7 +14,18 @@ public class InfoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_info, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_info, container, false);
 
+        GridView gridView = (GridView) view.findViewById(R.id.gridView);
+        GridAdapter adapter = new GridAdapter();
+        gridView.setAdapter(adapter);
+
+        adapter.addItem("북한산", R.drawable.mountain1);
+        adapter.addItem("남산", R.drawable.mountain3);
+        adapter.addItem("백두산", R.drawable.mountain2);
+        adapter.addItem("남산", R.drawable.mountain2);
+        adapter.addItem("백두산", R.drawable.mountain1);
+
+        return view;
+    }
 }
