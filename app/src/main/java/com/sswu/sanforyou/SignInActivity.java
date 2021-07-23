816af8afc4a263ea750c4d997abc2c803c41066d
Your Name <78153440+thisisyum@users.noreply.github.com>
2021-07-24 01:55:17 +0900

    feat: 로그인화면, 회원가입 화면 UI 수정 버튼 클릭 시 올바른 화면으로 이동하는 기능 추가

--- app/src/main/java/com/sswu/sanforyou/SignInActivity.java
+++ app/src/main/java/com/sswu/sanforyou/SignInActivity.java
@@ -9,8 +9,10 @@ import android.widget.Button;
 
 public class SignInActivity extends AppCompatActivity {
 
-    //로그인 버튼 : 회원가입 액티비티로 이동
+    //로그인 버튼 : Map Fragment로 이동
     private Button btn_sign_in;
+    //회원가입 버튼 : 회원가입 액티비티로 이동
+    private Button btn_sign_up;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +23,20 @@ public class SignInActivity extends AppCompatActivity {
         btn_sign_in.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(SignInActivity.this, SignUpActivity.class);
-                startActivity(intent); //액티비티 이동
+                Intent intent = new Intent(SignInActivity.this, MapFragment.class);
+                startActivity(intent);
             }
         });
+
+        btn_sign_up = findViewById(R.id.btn_sign_up);
+        btn_sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(SignInActivity.this, SignUpActivity.class);
+                startActivity(intent2); //액티비티 이동
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/sswu/sanforyou/SignUpActivity.java
+++ app/src/main/java/com/sswu/sanforyou/SignUpActivity.java
@@ -2,13 +2,28 @@ package com.sswu.sanforyou;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //회원가입 버튼 : 회원가입을 완료하고 Map Fragment로 넘어감
+    private Button btn_sign_up2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        btn_sign_up2 = findViewById(R.id.btn_sign_up2);
+        btn_sign_up2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpActivity.this, MapFragment.class );
+                startActivity(intent);
+            }
+        });
     }
 }
