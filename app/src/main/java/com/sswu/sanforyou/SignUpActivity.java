1bbc44067ff1075f998ccdca4898f38e8f46a4f3
Your Name <78153440+thisisyum@users.noreply.github.com>
2021-07-28 19:28:40 +0900

    feat: 회원가입 액티비티에 돌아가기 버튼 추가 Firebase, Realtime Database 연동해서 회원가입 기능 추가 사용자 정보 저장을 위한 UserAccount 클래스 추가

--- app/src/main/java/com/sswu/sanforyou/SignUpActivity.java
+++ app/src/main/java/com/sswu/sanforyou/SignUpActivity.java
@@ -1,29 +1,78 @@
 package com.sswu.sanforyou;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpActivity extends AppCompatActivity {
 
-    //회원가입 버튼 : 회원가입을 완료하고 Map Fragment로 넘어감
-    private Button btn_sign_up2;
+    private FirebaseAuth mFirebaseAuth; //파이어베이스 인증처리
+    private DatabaseReference mDatabaseRef; //실시간 데이터베이스
+    private EditText mEtEmail, mEtPwd; //회원가입 입력필드
+    private Button btn_sign_up2;  //회원가입 버튼
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("SanForYou");
+
+        mEtEmail = findViewById(R.id.et_email);
+        mEtPwd = findViewById(R.id.et_pwd);
+
         btn_sign_up2 = findViewById(R.id.btn_sign_up2);
         btn_sign_up2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(SignUpActivity.this, MapFragment.class );
+            public void onClick(View v) { //회원가입버튼 클릭 될때의 action
+
+                //회원가입 처리 시작
+                String strEmail = mEtEmail.getText().toString().trim();
+                String strPwd = mEtPwd.getText().toString().trim();
+
+                System.out.println("=====================================" + strEmail);
+                System.out.println("=====================================" + strPwd);
+
+                //FireBase Auth 진행
+                mFirebaseAuth.createUserWithEmailAndPassword(strEmail, strPwd).addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser();
+                            UserAccount account = new UserAccount();
+                            account.setIdToken(firebaseUser.getUid());
+                            account.setEmailId(firebaseUser.getEmail());
+                            account.setPassword(strPwd);
+
+                            //setValue : database에 insert!
+                            mDatabaseRef.child("UserAccount").child(firebaseUser.getUid()).setValue(account);
+
+                            Toast.makeText(SignUpActivity.this, "회원가입에 성공하셨습니다.", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(SignUpActivity.this, "회원가입에 실패하셨습니다.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+                Intent intent = new Intent(SignUpActivity.this, SignInActivity.class );
                 startActivity(intent);
             }
+
         });
     }
 }
